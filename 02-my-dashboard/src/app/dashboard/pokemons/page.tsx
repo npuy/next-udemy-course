67fa5e4745fd05858d9940c7f3c6a1dd2a004bd2
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-10-06 14:21:46 -0300

    Section 5

---
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,27 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/pokemons";
+
+const getPokemons = async (
+  limit = 151,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
+
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+
+  return (
+    <div className="flex flex-col">
+      <PokemonGrid pokemons={pokemons} />
+    </div>
+  );
+}
